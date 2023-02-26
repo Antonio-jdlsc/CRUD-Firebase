1525f50159687b214aa5c468538fd8cfd228384b
Antonio <antonio.jdlsc@gmail.com>
2023-02-26 17:40:00 -0600

    Setting the proyect basis

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import UserList from './components/UserList';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <UserList />
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
